584c774724e751ea8681a59b9c31d1735057ceaa
Colter Ulrich <cju5025@gmail.com>
2021-01-04 08:30:44 -0700

    Adds Event Listener to Nav Bar Links

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hit')
+
+
+
+$navBarLinks = document.querySelector('.nav-bar-links')
+
+$navBarLinks.addEventListener('mouseover', (event) => 
+{
+    $navBarLinks.classList.add('underline')
+})
+
+$navBarLinks.addEventListener('mouseout', (event) => 
+{
+    $navBarLinks.classList.remove('underline')
+})
