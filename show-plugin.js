363858b90034e84f8e6ecbdfdb817253bb99ec6f
Colter Ulrich <cju5025@gmail.com>
2021-01-06 15:39:28 -0700

    Dynamically appends plugin info to a show page.

--- show-plugin.js
+++ show-plugin.js
@@ -1,11 +1,30 @@
 const searchParams = new URLSearchParams(location.search)
 const id = searchParams.get('id')
 
-console.log(`id = ${id}`)
-
 fetch(`http://localhost:3000/items/${id}`)
     .then(response => response.json())
-    .then(console.log)
+    .then(item => showItem(item))
+
+    const $pluginSection = document.querySelector('#plugin-section')
+
+    function showItem (item) {
+        const $name = document.createElement('h1')
+        $name.id = "name"
+        $name.textContent = item.name
+
+        const $description = document.createElement('p')
+        $description.id = "description"
+        $description.textContent = item.description
+
+        const $image = document.createElement('img')
+        $image.id = "plugin-image"
+        $image.alt = item.name
+        $image.title = item.name 
+        $image.src = item.image_source
+
+        $pluginSection.append($name, $description, $image)
+    }
+
 
 
 
