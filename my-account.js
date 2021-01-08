9d87d704a9fa497a144cc3907a04c74384119db6
Colter Ulrich <cju5025@gmail.com>
2021-01-07 20:18:23 -0700

    Css facelift

--- my-account.js
+++ my-account.js
@@ -40,9 +40,9 @@ fetch('http://localhost:3000/purchased_items')
         $image.src = item.image_source
 
         const $downloadButton = document.createElement('a')
-        $downloadButton.id = "download-button"
+        $downloadButton.id = "download-link"
         $downloadButton.href = 'https://drive.google.com/u/0/uc?id=1WIiKPzflfFm1n2YL57Kfp8jWMikGz1JP&export=download'
-        $downloadButton.innerHTML = `<button type="button">Download</button>`
+        $downloadButton.innerHTML = `<button id="download-button">Download</button>`
 
         $pluginSection.append($name, $image, $downloadButton)
     }
